ab1f4290e5e719ff6caaef8626ca5c6e5e97e382
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-09-09 09:56:15 +0330

    update

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+include("langs.php");
+if(isset($_GET["lang"])){
+    $langCode = $_GET["lang"];
+} else {
+    $langCode = "en";
+}
+?>
+<!doctype html>
+<html lang="en" dir="<?php echo $lang[$langCode]['dir']; ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $lang[$langCode]['title']; ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <h1><?php echo $lang[$langCode]['description']; ?></h1>
+
+    <a href="index.php?lang=en" class="btn btn-primary">EN</a>
+    <a href="index.php?lang=fa" class="btn btn-primary">FA</a>
+    <div class="row">
+        <?php foreach ($lang[$langCode]['products'] as $pid => $product): ?>
+        <div class="col-3">
+            <div class="card">
+                <img src="<?php echo $product['images']; ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $product['name']; ?></h5>
+                    <p class="card-text"><?php echo $product['price']; ?></p>
+                    <a href="detail.php?product_id=<?php echo $pid; ?>" class="btn btn-primary"><?php echo $lang[$langCode]['detail']; ?></a>
+                </div>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ langs.php
@@ -0,0 +1,63 @@
+<?php
+$lang = array();
+
+$lang['fa']['title'] = "لوازم آرایشی عسل";
+$lang['fa']['description'] = "خرید با بهترین قیمت";
+$lang['fa']['dir'] = "rtl";
+$lang['fa']['detail'] = "توضیحات بیشتر";
+
+$lang['fa']['products'][0]['name'] = "آب رسان";
+$lang['fa']['products'][0]['price'] = 1800;
+$lang['fa']['products'][0]['images'] = 'images/1.jpg';
+$lang['fa']['products'][0]['des'] = 'فارسی فارسی';
+
+$lang['fa']['products'][1]['name'] = "عطر";
+$lang['fa']['products'][1]['price'] = 3500;
+$lang['fa']['products'][1]['images'] = 'images/2.jpg';
+$lang['fa']['products'][1]['des'] = 'فارسی فارسی';
+
+$lang['fa']['products'][2]['name'] = "کانسیلر";
+$lang['fa']['products'][2]['price'] = 1800;
+$lang['fa']['products'][2]['images'] = 'images/3.jpg';
+$lang['fa']['products'][2]['des'] = 'فارسی فارسی';
+
+$lang['fa']['products'][3]['name'] = "فیکس";
+$lang['fa']['products'][3]['price'] = 800;
+$lang['fa']['products'][3]['images'] = 'images/4.jpg';
+$lang['fa']['products'][3]['des'] = 'فارسی فارسی';
+
+$lang['fa']['products'][4]['name'] = "ریمل";
+$lang['fa']['products'][4]['price'] = 200;
+$lang['fa']['products'][4]['images'] = 'images/5.jpg';
+$lang['fa']['products'][4]['des'] = 'فارسی فارسی';
+
+
+$lang['en']['title'] = "Beauty Accessories Asal";
+$lang['en']['description'] = "Best price for products";
+$lang['en']['dir'] = "ltr";
+$lang['en']['detail'] = "Read more";
+
+$lang['en']['products'][0]['name'] = "Ab resan";
+$lang['en']['products'][0]['price'] = 1800;
+$lang['en']['products'][0]['images'] = 'images/1.jpg';
+$lang['en']['products'][0]['des'] = 'English English English ';
+
+$lang['en']['products'][1]['name'] = "Pefuom";
+$lang['en']['products'][1]['price'] = 3500;
+$lang['en']['products'][1]['images'] = 'images/2.jpg';
+$lang['en']['products'][1]['des'] = 'English English English ';
+
+$lang['en']['products'][2]['name'] = "Consiler";
+$lang['en']['products'][2]['price'] = 1800;
+$lang['en']['products'][2]['images'] = 'images/3.jpg';
+$lang['en']['products'][2]['des'] = 'English English English ';
+
+$lang['en']['products'][3]['name'] = "FIx";
+$lang['en']['products'][3]['price'] = 800;
+$lang['en']['products'][3]['images'] = 'images/4.jpg';
+$lang['en']['products'][3]['des'] = 'English English English ';
+
+$lang['en']['products'][4]['name'] = "Rimel";
+$lang['en']['products'][4]['price'] = 200;
+$lang['en']['products'][4]['images'] = 'images/5.jpg';
+$lang['en']['products'][4]['des'] = 'English English English ';
